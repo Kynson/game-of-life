cf379721248cd9adc8aae8f5df8420db095f4150
Kynson Szetau <46522440+Kynson@users.noreply.github.com>
2024-07-27 18:17:16 +0800

    Fix cf page build script

--- scripts/cf-pages-build.sh
+++ scripts/cf-pages-build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-curl https://sh.rustup.rs -sSf | sh -- -y && . "$HOME/.cargo/env"
+curl https://sh.rustup.rs -sSf | sh -s -- -y && . "$HOME/.cargo/env"
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 node scripts/build-all.mjs
